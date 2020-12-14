1631406ff612684b0d8deda9b420bb6df42e3689
Matthew Igbo <mattew.igbo@thebulb.africa>
2020-12-14 12:01:36 +0100

    Final Update

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,32 +14,32 @@ namespace MatricesWithArrays
     {
         public static int rows;
         public static int columns;
-        public static int[,] tbt;
         public static int[,] twdMatrix = new int[rows, columns];
         public static int[,,,] fdMatrix = new int[1, 1, 4, 4];
         public static Random random = new Random();
-        public static int i, j, k, l;
 
         public static void Main(string[] args)
         {
             try
             {
-                Console.WriteLine("This is a program that generates a Matrix and then determines the determinants and square!");
+                Console.WriteLine("This is a program that generates a square matrix and then determines the determinants and square!");
                 Console.WriteLine("Select your preferred Matrix");
                 Console.WriteLine("(1) 2D Matrix (2) 4D Matrix(DEMO)");
-                int matrix = int.Parse(Console.ReadLine());
-                if (matrix == 1)
+                int dimensionChoice = int.Parse(Console.ReadLine());
+                if (dimensionChoice == 1)
                 {
                     Console.WriteLine("2D Matrix Selected!");
                     Console.WriteLine("Enter Dimensions for 2D square matrix( Not more than 4. i.e 2x2, 3x3, 4x4 )");
-                    Matrix2DGen();
+                    DisplayMatrixGen(twdMatrix);
+                    
                 }
-                else if (matrix == 2)
+                else if (dimensionChoice == 2)
                 {
                     Console.WriteLine("4D Matrix Selected!");
                     Console.WriteLine("Generating and Populating Matrix with random elements...");
-                    Display4DMatrixGen(fdMatrix);
+                    DisplayMatrixGen(fdMatrix);
                     Matrix4DDeterminant(fdMatrix);
+                    SquaredMatrix(fdMatrix);
                 }
                 else
                 {
@@ -51,10 +51,12 @@ namespace MatricesWithArrays
             {
                 Console.WriteLine(e.Message);
             }
+            Console.ReadLine();
+
 
         }
 
-        public static void Display4DMatrixGen(int[,,,] array)//Generates a 4 Dimensional Matrix of dimensions [1,1,4,4], with it's elements randomly generated.
+        public static void DisplayMatrixGen(int[,,,] array)//Generates a 4 Dimensional Matrix of dimensions [1,1,4,4], with it's elements randomly generated.
         {
             for (var i = 0; i < array.GetLength(0); i++)
             {
@@ -66,8 +68,8 @@ namespace MatricesWithArrays
                     {
                         for (var l = 0; l < array.GetLength(3); l++)
                         {
-                            fdMatrix[i, j, k, l] = random.Next(0, 20);   //Randomly generates elements in the matrix
-                            Console.Write($"  { fdMatrix[i, j, k, l] }");
+                            array[i, j, k, l] = random.Next(0, 20);   //Randomly generates elements in the matrix
+                            Console.Write($"  { array[i, j, k, l] }");
                         }
                         Console.WriteLine();
                     }
@@ -78,7 +80,7 @@ namespace MatricesWithArrays
 
         }
 
-        public static void Matrix2DGen()//Generates a 2 Dimensional Square Matrix
+        public static void DisplayMatrixGen(int[,] array)//Generates a 2 Dimensional Square Matrix
         {
             try
             {
@@ -87,7 +89,7 @@ namespace MatricesWithArrays
                 Console.WriteLine("Enter number of Columns: ");
                 columns = int.Parse(Console.ReadLine());
 
-                if (rows == 1 && columns == 1 || rows > 4 && columns > 4)
+                if (rows == 1 && columns == 1 || rows > 4 && columns > 4)//Checks if row and column entered is 1 or greater than 4
                 {
                     Console.WriteLine("Invalid Dimensions Selected");
                     Console.WriteLine("Dimensions entered do not match required input!");
@@ -105,7 +107,7 @@ namespace MatricesWithArrays
                     return;
                 }
                 
-                twdMatrix = new int[rows, columns];
+                twdMatrix = new int[rows, columns];//Generates square matrix with elements populated
                 for (var i = 0; i < rows; i++)
                 {
                     Console.WriteLine("{");
@@ -119,15 +121,18 @@ namespace MatricesWithArrays
                 }
                 if (rows == 2 && columns == 2)
                 {
-                    tbt = Matrix2x2Determinant(twdMatrix);
+                    Matrix2x2Determinant(twdMatrix);
+                    //SquaredMatrix(twdMatrix);
                 }
                 else if (rows == 3 && columns == 3)
                 {
-                    tbt = Matrix3x3Determinant(twdMatrix);
+                    Matrix3x3Determinant(twdMatrix);
+                    //SquaredMatrix(twdMatrix);
                 }
                 else if (rows == 4 && columns == 4)
                 {
-                    tbt = Matrix3x3Determinant(twdMatrix);
+                    Matrix3x3Determinant(twdMatrix);
+                    //SquaredMatrix(twdMatrix);
                 }
                 else
                 {
@@ -142,50 +147,71 @@ namespace MatricesWithArrays
 
         }
 
+        
         //Matrix Determinants
         public static void Matrix4DDeterminant(int[,,,] array)//4D Matrix Determinant method
         {
-            var determinant = +fdMatrix[0, 0, 0, 0] * (fdMatrix[0, 0, 1, 1] * fdMatrix[0, 0, 2, 2] * fdMatrix[0, 0, 3, 3] - fdMatrix[0, 0, 1, 3] * fdMatrix[0, 0, 2, 2] * fdMatrix[0, 0, 3, 1])
-            - fdMatrix[0, 0, 0, 1] * (fdMatrix[0, 0, 1, 0] * fdMatrix[0, 0, 2, 2] * fdMatrix[0, 0, 3, 3] - fdMatrix[0, 0, 1, 3] * fdMatrix[0, 0, 2, 2] * fdMatrix[0, 0, 3, 0])
-            + fdMatrix[0, 0, 0, 2] * (fdMatrix[0, 0, 1, 0] * fdMatrix[0, 0, 2, 1] * fdMatrix[0, 0, 3, 3] - fdMatrix[0, 0, 1, 3] * fdMatrix[0, 0, 2, 1] * fdMatrix[0, 0, 3, 1])
-            - fdMatrix[0, 0, 0, 3] * (fdMatrix[0, 0, 1, 0] * fdMatrix[0, 0, 2, 1] * fdMatrix[0, 0, 3, 2] - fdMatrix[0, 0, 1, 2] * fdMatrix[0, 0, 2, 1] * fdMatrix[0, 0, 3, 0]);
+            var determinant = +array[0, 0, 0, 0] * (array[0, 0, 1, 1] * array[0, 0, 2, 2] * array[0, 0, 3, 3] - array[0, 0, 1, 3] * array[0, 0, 2, 2] * array[0, 0, 3, 1])
+            - array[0, 0, 0, 1] * (array[0, 0, 1, 0] * array[0, 0, 2, 2] * array[0, 0, 3, 3] - array[0, 0, 1, 3] * array[0, 0, 2, 2] * array[0, 0, 3, 0])
+            + array[0, 0, 0, 2] * (array[0, 0, 1, 0] * array[0, 0, 2, 1] * array[0, 0, 3, 3] - array[0, 0, 1, 3] * array[0, 0, 2, 1] * array[0, 0, 3, 1])
+            - array[0, 0, 0, 3] * (array[0, 0, 1, 0] * array[0, 0, 2, 1] * array[0, 0, 3, 2] - array[0, 0, 1, 2] * array[0, 0, 2, 1] * array[0, 0, 3, 0]);
 
             Console.WriteLine($"The determinant of the matrix is {determinant}");
 
         }
 
-        public static int[,] Matrix2x2Determinant(int[,] array)//2D (2x2) Matrix Determinant method
+        public static void Matrix2x2Determinant(int[,] array)//2D (2x2) Matrix Determinant method
         {
-            var determinant = (twdMatrix[0, 0] * twdMatrix[1, 1] - twdMatrix[0, 0] * twdMatrix[0, 1] * twdMatrix[1, 0]);
+            var determinant = (array[0, 0] * array[1, 1] - array[0, 0] * array[0, 1] * array[1, 0]);
 
             Console.WriteLine($"The determinant of the matrix is {determinant}");
-            return array;
 
         }
 
-        public static int[,] Matrix3x3Determinant(int[,] array)//2D (3x3) Matrix Determinant method
+        public static void Matrix3x3Determinant(int[,] array)//2D (3x3) Matrix Determinant method
         {
-            var determinant = +twdMatrix[0, 0] * (twdMatrix[1, 1] * twdMatrix[2, 2] - twdMatrix[1, 2] * twdMatrix[2, 1])
-                              - twdMatrix[0, 1] * (twdMatrix[1, 0] * twdMatrix[2, 2] - twdMatrix[1, 2] * twdMatrix[2, 0])
-                              + twdMatrix[0, 2] * (twdMatrix[1, 0] * twdMatrix[2, 1] - twdMatrix[1, 1] * twdMatrix[2, 0]);
+            var determinant = +array[0, 0] * (array[1, 1] * array[2, 2] - array[1, 2] * array[2, 1])
+                              - array[0, 1] * (array[1, 0] * array[2, 2] - array[1, 2] * array[2, 0])
+                              + array[0, 2] * (array[1, 0] * array[2, 1] - array[1, 1] * array[2, 0]);
 
             Console.WriteLine($"The determinant of the matrix is {determinant}");
-            return array;
 
         }
 
-        public static int[,] Matrix4x4Determinant(int[,] array)//2D (4x4) Matrix Determinant method
+        public static void Matrix4x4Determinant(int[,] array)//2D (4x4) Matrix Determinant method
         {
-            var determinant = +twdMatrix[0, 0] * (twdMatrix[1, 1] * twdMatrix[2, 2] * twdMatrix[3, 3] - twdMatrix[1, 3] * twdMatrix[2, 2] * twdMatrix[3, 1])
-            - twdMatrix[0, 1] * (twdMatrix[1, 0] * twdMatrix[2, 2] * twdMatrix[3, 3] - twdMatrix[1, 3] * twdMatrix[2, 2] * twdMatrix[3, 0])
-            + twdMatrix[0, 2] * (twdMatrix[1, 0] * twdMatrix[2, 1] * twdMatrix[3, 3] - twdMatrix[1, 3] * twdMatrix[2, 1] * twdMatrix[3, 1])
-            - twdMatrix[0, 3] * (twdMatrix[1, 0] * twdMatrix[2, 1] * twdMatrix[3, 2] - twdMatrix[1, 2] * twdMatrix[2, 1] * twdMatrix[3, 0]);
+            var determinant = +array[0, 0] * (array[1, 1] * array[2, 2] * array[3, 3] - array[1, 3] * array[2, 2] * array[3, 1])
+            - array[0, 1] * (array[1, 0] * array[2, 2] * array[3, 3] - array[1, 3] * array[2, 2] * array[3, 0])
+            + array[0, 2] * (array[1, 0] * array[2, 1] * array[3, 3] - array[1, 3] * array[2, 1] * array[3, 1])
+            - array[0, 3] * (array[1, 0] * array[2, 1] * array[3, 2] - array[1, 2] * array[2, 1] * array[3, 0]);
 
             Console.WriteLine($"The determinant of the matrix is {determinant}");
-            return array;
-
         }
 
+        public static void SquaredMatrix(int[,,,] array)
+        {
+            var rowcount = rows;
+            var columncount = columns;
+            int[,,,] arrayCopy = (int[,,,])array.Clone();
+            int[,,,] arrayProduct = (int[,,,])array.Clone();
+            for (var i = 0; i < rowcount; i++)
+            {
+                for (var j = 0; j < arrayCopy.GetLength(1); j++)
+                {
+                    var product = 0;
+                    for (var k = 0; k < columncount; k++)
+                    {
+                        var a = array[0,0,i, k];
+                        var b = arrayCopy[0,0,k, j];
+                        product += a * b;
+                    }
+                    arrayProduct[0,0,i, j] = product;
+                }
+            }
+            Console.WriteLine($"The square of the matrix is ");
+            DisplayMatrixGen(arrayProduct);
+        }
+        
 
     }
 }
